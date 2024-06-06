222139d4d98299c9759cc5bc8b3169301b86fb50
Leonard Paturel <leonardpaturel@gmail.com>
2024-06-06 09:30:51 +0100

    cleanup lib

--- lib/index.ts
+++ lib/index.ts
@@ -10,17 +10,11 @@ export * from "./contracts";
 export * from "./devnet";
 export * from "./expectations";
 export * from "./manager";
-export * from "./multisig";
 export * from "./openZeppelinAccount";
-export * from "./outsideExecution";
 export * from "./receipts";
-export * from "./recovery";
 export * from "./signers/legacy";
-export * from "./signers/secp256";
 export * from "./signers/signers";
-export * from "./signers/webauthn";
 export * from "./tokens";
-export * from "./udc";
 export * from "./upgrade";
 
 export type Constructor<T> = new (...args: any[]) => T;
