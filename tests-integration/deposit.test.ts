84a0876256d2c34ac7a3665f89120bf8aba52e5f
Leonard Paturel <leonardpaturel@gmail.com>
2024-06-17 15:11:31 +0200

    deposit tests

--- tests-integration/deposit.test.ts
+++ tests-integration/deposit.test.ts
@@ -1,26 +1,36 @@
 import { expect } from "chai";
-import {
-  calculateClaimAddress,
-  claimExternal,
-  defaultDepositTestSetup,
-  manager,
-  randomReceiver,
-  setupGiftProtocol,
-} from "../lib";
+import { calculateClaimAddress, defaultDepositTestSetup, deployMockERC20, manager, setupGiftProtocol } from "../lib";
 
 describe("Deposit", function () {
   for (const useTxV3 of [false, true]) {
-    it(`Testing claim_external flow using txV3: ${useTxV3} (no dust receiver)`, async function () {
+    it(`Deposit works using: ${useTxV3} (gift token == claim token)`, async function () {
       const { factory } = await setupGiftProtocol();
-      const { claim, claimPrivateKey } = await defaultDepositTestSetup({ factory });
-      const receiver = randomReceiver();
+
+      const { claim } = await defaultDepositTestSetup({ factory, useTxV3 });
+
       const claimAddress = calculateClaimAddress(claim);
 
-      await claimExternal({ claim, receiver, claimPrivateKey });
+      const giftTokenBalance = await manager.tokens.tokenBalance(claimAddress, claim.gift_token);
+      expect(giftTokenBalance == claim.gift_amount + claim.fee_amount).to.be.true;
+    });
 
-      const finalBalance = await manager.tokens.tokenBalance(claimAddress, claim.gift_token);
-      expect(finalBalance == claim.fee_amount).to.be.true;
-      await manager.tokens.tokenBalance(receiver, claim.gift_token).should.eventually.equal(claim.gift_amount);
+    it(`Deposit works using: ${useTxV3} (gift token != claim token)`, async function () {
+      const { factory } = await setupGiftProtocol();
+      const giftToken = await deployMockERC20();
+
+      const { claim } = await defaultDepositTestSetup({
+        factory,
+        useTxV3,
+        overrides: { giftTokenAddress: giftToken.address },
+      });
+
+      const claimAddress = calculateClaimAddress(claim);
+
+      const giftTokenBalance = await manager.tokens.tokenBalance(claimAddress, claim.gift_token);
+      expect(giftTokenBalance == claim.gift_amount).to.be.true;
+
+      const feeTokenBalance = await manager.tokens.tokenBalance(claimAddress, claim.fee_token);
+      expect(feeTokenBalance == claim.fee_amount).to.be.true;
     });
   }
 });
