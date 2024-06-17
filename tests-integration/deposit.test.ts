a64f9a368f6ce63109e4d1d902510e6ce3b4d440
Leonard Paturel <leonardpaturel@gmail.com>
2024-06-17 12:52:33 +0200

    start deposit tests

--- tests-integration/deposit.test.ts
+++ tests-integration/deposit.test.ts
@@ -1,1 +1,26 @@
-// deposit tests
+import { expect } from "chai";
+import {
+  calculateClaimAddress,
+  claimExternal,
+  defaultDepositTestSetup,
+  manager,
+  randomReceiver,
+  setupGiftProtocol,
+} from "../lib";
+
+describe("Deposit", function () {
+  for (const useTxV3 of [false, true]) {
+    it(`Testing claim_external flow using txV3: ${useTxV3} (no dust receiver)`, async function () {
+      const { factory } = await setupGiftProtocol();
+      const { claim, claimPrivateKey } = await defaultDepositTestSetup(factory);
+      const receiver = randomReceiver();
+      const claimAddress = calculateClaimAddress(claim);
+
+      await claimExternal({ claim, receiver, claimPrivateKey });
+
+      const finalBalance = await manager.tokens.tokenBalance(claimAddress, claim.gift_token);
+      expect(finalBalance == claim.fee_amount).to.be.true;
+      await manager.tokens.tokenBalance(receiver, claim.gift_token).should.eventually.equal(claim.gift_amount);
+    });
+  }
+});
