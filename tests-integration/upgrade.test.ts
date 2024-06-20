33eec92da15b0a41f7d689d8c9ebe939ce52ee6e
Leonard Paturel <leonardpaturel@gmail.com>
2024-06-20 16:36:58 +0100

    fix time

--- tests-integration/upgrade.test.ts
+++ tests-integration/upgrade.test.ts
@@ -1,6 +1,6 @@
 import { deployer, manager, setupGiftProtocol } from "../lib";
 
-const MIN_SECURITY_PERIOD = 172800; // 7 * 24 * 60 * 60;  // 7 day
+const MIN_SECURITY_PERIOD = 604800; // 7 * 24 * 60 * 60;  // 7 day
 
 ///  Time window during which the upgrade can be performed
 const VALID_WINDOW_PERIOD = 604800; // 7 * 24 * 60 * 60;  // 7 days
@@ -8,14 +8,14 @@ const VALID_WINDOW_PERIOD = 604800; // 7 * 24 * 60 * 60;  // 7 days
 const CURRENT_TIME = 100;
 
 describe("Test  Factory Upgrade", function () {
-  it("Upgrade", async function () {
+  it.only("Upgrade", async function () {
     const { factory } = await setupGiftProtocol();
     const newFactoryClassHash = await manager.declareFixtureContract("GiftFactoryUpgrade");
 
+    await manager.setTime(CURRENT_TIME);
     factory.connect(deployer);
     await factory.propose_upgrade(newFactoryClassHash);
 
-    await manager.setTime(CURRENT_TIME);
     await factory.get_upgrade_ready_at().should.eventually.equal(BigInt(CURRENT_TIME + MIN_SECURITY_PERIOD));
     await factory.get_proposed_implementation().should.eventually.equal(BigInt(newFactoryClassHash));
 
