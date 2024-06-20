739617fadbdac6e99317a0b9b426c5a783baee83
Leonard Paturel <leonardpaturel@gmail.com>
2024-06-20 13:57:53 +0100

    remove upgrade fn

--- lib/protocol.ts
+++ lib/protocol.ts
@@ -37,5 +37,3 @@ export async function setupGiftProtocol(): Promise<{
   cache["GiftFactory"] = factory;
   return { factory, claimAccountClassHash };
 }
-
-export async function upgradeFactory(factory: Contract, owner = deployer) {}
