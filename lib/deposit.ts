0c580cdb788820e3c5432c9ad471c958524db85d
delaaxe <1091900+delaaxe@users.noreply.github.com>
2024-08-07 16:46:52 +0200

    Add scripts for creating and claiming gifts

--- lib/deposit.ts
+++ lib/deposit.ts
@@ -1,10 +1,61 @@
 import { Account, Call, CallData, Contract, InvokeFunctionResponse, TransactionReceipt, hash, uint256 } from "starknet";
-import { AccountConstructorArguments, Gift, LegacyStarknetKeyPair, deployer, manager } from "./";
+import { AccountConstructorArguments, Gift, LegacyStarknetKeyPair, deployer, manager } from ".";
 
 export const STRK_GIFT_MAX_FEE = 200000000000000000n; // 0.2 STRK
 export const STRK_GIFT_AMOUNT = STRK_GIFT_MAX_FEE + 1n;
 export const ETH_GIFT_MAX_FEE = 200000000000000n; // 0.0002 ETH
 export const ETH_GIFT_AMOUNT = ETH_GIFT_MAX_FEE + 1n;
+
+const depositAbi = [
+  {
+    type: "function",
+    name: "deposit",
+    inputs: [
+      {
+        name: "escrow_class_hash",
+        type: "core::starknet::class_hash::ClassHash",
+      },
+      {
+        name: "gift_token",
+        type: "core::starknet::contract_address::ContractAddress",
+      },
+      {
+        name: "gift_amount",
+        type: "core::integer::u256",
+      },
+      {
+        name: "fee_token",
+        type: "core::starknet::contract_address::ContractAddress",
+      },
+      {
+        name: "fee_amount",
+        type: "core::integer::u128",
+      },
+      {
+        name: "gift_pubkey",
+        type: "core::felt252",
+      },
+    ],
+    outputs: [],
+    state_mutability: "external",
+  },
+];
+
+const approveAbi = [
+  {
+    type: "function",
+    name: "approve",
+    inputs: [
+      {
+        name: "spender",
+        type: "core::starknet::contract_address::ContractAddress",
+      },
+      { name: "amount", type: "core::integer::u256" },
+    ],
+    outputs: [{ type: "core::bool" }],
+    state_mutability: "external",
+  },
+];
 
 export function getMaxFee(useTxV3: boolean): bigint {
   return useTxV3 ? STRK_GIFT_MAX_FEE : ETH_GIFT_MAX_FEE;
@@ -14,42 +65,37 @@ export function getGiftAmount(useTxV3: boolean): bigint {
   return useTxV3 ? STRK_GIFT_AMOUNT : ETH_GIFT_AMOUNT;
 }
 
-export async function deposit(depositParams: {
-  sender: Account;
+interface DepositParams {
   giftAmount: bigint;
   feeAmount: bigint;
   factoryAddress: string;
   feeTokenAddress: string;
   giftTokenAddress: string;
   giftSignerPubKey: bigint;
-  overrides?: {
-    escrowAccountClassHash?: string;
-  };
-}): Promise<{ response: InvokeFunctionResponse; gift: Gift }> {
-  const { sender, giftAmount, feeAmount, factoryAddress, feeTokenAddress, giftTokenAddress, giftSignerPubKey } =
-    depositParams;
-  const factory = await manager.loadContract(factoryAddress);
-  const feeToken = await manager.loadContract(feeTokenAddress);
-  const giftToken = await manager.loadContract(giftTokenAddress);
+  escrowAccountClassHash: string;
+}
 
-  const escrowAccountClassHash =
-    depositParams.overrides?.escrowAccountClassHash || (await factory.get_latest_escrow_class_hash());
-  const gift: Gift = {
-    factory: factoryAddress,
-    escrow_class_hash: escrowAccountClassHash,
-    sender: deployer.address,
-    gift_token: giftTokenAddress,
-    gift_amount: giftAmount,
-    fee_token: feeTokenAddress,
-    fee_amount: feeAmount,
-    gift_pubkey: giftSignerPubKey,
-  };
-  const calls: Array<Call> = [];
+export function createDeposit(
+  sender: string,
+  {
+    giftAmount,
+    feeAmount,
+    factoryAddress,
+    feeTokenAddress,
+    giftTokenAddress,
+    giftSignerPubKey,
+    escrowAccountClassHash,
+  }: DepositParams,
+) {
+  const factory = new Contract(depositAbi, factoryAddress);
+  const feeToken = new Contract(approveAbi, feeTokenAddress);
+  const giftToken = new Contract(approveAbi, giftTokenAddress);
+  const calls: Call[] = [];
   if (feeTokenAddress === giftTokenAddress) {
-    calls.push(feeToken.populateTransaction.approve(factory.address, giftAmount + feeAmount));
+    calls.push(feeToken.populateTransaction.approve(factoryAddress, giftAmount + feeAmount));
   } else {
-    calls.push(feeToken.populateTransaction.approve(factory.address, feeAmount));
-    calls.push(giftToken.populateTransaction.approve(factory.address, giftAmount));
+    calls.push(feeToken.populateTransaction.approve(factoryAddress, feeAmount));
+    calls.push(giftToken.populateTransaction.approve(factoryAddress, giftAmount));
   }
   calls.push(
     factory.populateTransaction.deposit(
@@ -61,10 +107,26 @@ export async function deposit(depositParams: {
       giftSignerPubKey,
     ),
   );
-  return {
-    response: await sender.execute(calls),
-    gift,
+  const gift: Gift = {
+    factory: factoryAddress,
+    escrow_class_hash: escrowAccountClassHash,
+    sender,
+    gift_token: giftTokenAddress,
+    gift_amount: giftAmount,
+    fee_token: feeTokenAddress,
+    fee_amount: feeAmount,
+    gift_pubkey: giftSignerPubKey,
   };
+  return { calls, gift };
+}
+
+export async function deposit(
+  sender: Account,
+  depositParams: DepositParams,
+): Promise<{ response: InvokeFunctionResponse; gift: Gift }> {
+  const { calls, gift } = createDeposit(sender.address, depositParams);
+  const response = await sender.execute(calls);
+  return { response, gift };
 }
 
 export async function defaultDepositTestSetup(args: {
@@ -97,15 +159,14 @@ export async function defaultDepositTestSetup(args: {
   const giftSigner = new LegacyStarknetKeyPair(args.overrides?.giftPrivateKey);
   const giftPubKey = giftSigner.publicKey;
 
-  const { response, gift } = await deposit({
-    sender: deployer,
-    overrides: { escrowAccountClassHash },
+  const { response, gift } = await deposit(deployer, {
     giftAmount,
     feeAmount,
     factoryAddress: args.factory.address,
     feeTokenAddress: feeToken.address,
     giftTokenAddress,
     giftSignerPubKey: giftPubKey,
+    escrowAccountClassHash,
   });
   const txReceipt = await manager.waitForTransaction(response.transaction_hash);
   return { gift, giftPrivateKey: giftSigner.privateKey, txReceipt };
@@ -121,14 +182,7 @@ export function calculateEscrowAddress(gift: Gift): string {
     gift_pubkey: gift.gift_pubkey,
   };
 
-  const escrowAddress = hash.calculateContractAddressFromHash(
-    0,
-    gift.escrow_class_hash,
-    CallData.compile({
-      ...constructorArgs,
-      gift_amount: uint256.bnToUint256(gift.gift_amount),
-    }),
-    gift.factory,
-  );
+  const calldata = CallData.compile({ ...constructorArgs, gift_amount: uint256.bnToUint256(gift.gift_amount) });
+  const escrowAddress = hash.calculateContractAddressFromHash(0, gift.escrow_class_hash, calldata, gift.factory);
   return escrowAddress;
 }
